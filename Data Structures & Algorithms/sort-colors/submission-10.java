3e820609b53f3dd01bb0374536f73573225666fd
Tousif Rahaman <tousifrahamanwork12@gmail.com>
2026-06-25 20:13:03 +0530

    Add: sort-colors - submission-10

---
+++ Data Structures & Algorithms/sort-colors/submission-10.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int i = 0, j = 0, r = nums.length - 1;
+         while (j <= r) {
+            if (nums[j] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j++;
+            } 
+            else if (nums[j] == 2) {
+                int temp = nums[j];
+                nums[j] = nums[r];
+                nums[r] = temp;
+                 r--;
+            } 
+            else {
+                j++;
+            }
+        }
+    }
+}
